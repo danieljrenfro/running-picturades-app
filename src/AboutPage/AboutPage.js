abb2962643cf5bddb4c2c8eb82fc4e6c82179a79
danieljrenfro <djrenfro95@gmail.com>
2021-01-13 15:07:14 -0600

    Added login instructions

--- src/AboutPage/AboutPage.js
+++ src/AboutPage/AboutPage.js
@@ -14,6 +14,12 @@ class AboutPage extends Component {
         <p>Running Picturades is a game with 2-4 teams. In every game you have a Word Giver who creates a list of words, decides whether it will be pictionary or charades and administers those words to the teams. At the beginning, each team will send one player to the Word Giver. The Word Giver will tell each player the initial word. The players run back to their teams and begin either acting or drawing depending on the game type.</p>
 
         <p>Whenever someone guesses the correct word they will run to the Word Giver to check that it was correct, receive the next word and run back to try and get their team to guess the next word! This cycle continues until whichever team moves through all of the words in the list and wins!</p>
+
+        <h2 className="about-header">Getting Setup</h2>
+
+        <p>Getting started with Running Picturades app is super easy! To begin a game all you have to do is go to the Lists section and then select a list to start a game!</p> 
+
+        <p>If you want to create your own lists, you will need to login to an account. You can either register for a new account or you can sign into 'testUser' with password: 'Password1!'.</p>
         
         <h2 className="about-header">How to Play</h2>
         
