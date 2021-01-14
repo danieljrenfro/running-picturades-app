c5be6ccaf96fcfdf074f63052ed55197ecc56b2a
danieljrenfro <djrenfro95@gmail.com>
2021-01-14 16:01:17 -0600

    updated README file

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Running Picturades App
 [Running Picturades App](https://running-picturades-app.vercel.app/)
 
+## Purpose of the App
+
+Running Picturades is a game with 2 teams. In every game you have a Word Giver who creates a list of words, decides whether it will be pictionary or charades and administers those words to the teams. At the beginning, each team will send one player to the Word Giver. The Word Giver will tell each player the initial word. The players run back to their teams and begin either acting or drawing depending on the game type.
+
+Whenever someone guesses the correct word they will run to the Word Giver to check that it was correct, receive the next word and run back to try and get their team to guess the next word! This cycle continues until whichever team moves through all of the words in the list and wins!
+
+The purpose of the Running Picturades App is to assist the person who is managing a game of running pictionary or charades. The user will be able to create, edit, delete and see all lists. The user will be able to start a game from a list and administer the words to each team from that view.
+
 ***
 
 ## Tour of the App
--- src/AboutPage/AboutPage.js
+++ src/AboutPage/AboutPage.js
@@ -11,7 +11,7 @@ class AboutPage extends Component {
       <section className="app-instructions">
         <h2 className="about-header">What is Running Picturades?</h2>
         
-        <p>Running Picturades is a game with 2-4 teams. In every game you have a Word Giver who creates a list of words, decides whether it will be pictionary or charades and administers those words to the teams. At the beginning, each team will send one player to the Word Giver. The Word Giver will tell each player the initial word. The players run back to their teams and begin either acting or drawing depending on the game type.</p>
+        <p>Running Picturades is a game with 2 teams. In every game you have a Word Giver who creates a list of words, decides whether it will be pictionary or charades and administers those words to the teams. At the beginning, each team will send one player to the Word Giver. The Word Giver will tell each player the initial word. The players run back to their teams and begin either acting or drawing depending on the game type.</p>
 
         <p>Whenever someone guesses the correct word they will run to the Word Giver to check that it was correct, receive the next word and run back to try and get their team to guess the next word! This cycle continues until whichever team moves through all of the words in the list and wins!</p>
 
@@ -19,7 +19,11 @@ class AboutPage extends Component {
 
         <p>Getting started with Running Picturades app is super easy! To begin a game all you have to do is go to the Lists section and then select a list to start a game!</p> 
 
-        <p>If you want to create your own lists, you will need to login to an account. You can either register for a new account or you can sign into 'testUser' with password: 'Password1!'.</p>
+        <p>If you want to create your own lists, you will need to register and log in to an account.</p>
+
+        <h3>Demo User</h3>
+        <p>Username: testUser</p>
+        <p>Password: Password1!</p>
         
         <h2 className="about-header">How to Play</h2>
         
